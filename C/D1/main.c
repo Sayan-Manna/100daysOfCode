ce34cc46e9baadbfa25649e3d73da6d8089f132e
Sayan Manna <53704744+Sayan-Manna@users.noreply.github.com>
2020-01-26 21:43:48 +0530

    c

---
+++ C/D1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    float salary = 56.23;
+    a = 8;
+    b = 34;
+    char charName = 'Z';
+    int c = a+b;
+    printf("%d\n",c);
+    printf("%f\n",salary);
+    printf("%c\n",charName);
+    
+    return 0; 
+
+}
