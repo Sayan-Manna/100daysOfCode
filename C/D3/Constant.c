bbb10147d4e810382b1bc777a241843d54be4471
Sayan Manna <53704744+Sayan-Manna@users.noreply.github.com>
2020-01-29 00:08:06 +0530

    D3

---
+++ C/D3/Constant.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+   /* const double PI = 3.14;*/
+    printf("%f",PI);
+
+
+    return 0;
+}
---
+++ C/D3/OutPut.c
@@ -0,0 +1,71 @@
+// putchar() :
+
+#include<stdio.h>
+int main()
+/*{
+    /*char a  = getchar();
+    printf("You entered: ");
+    putchar(a);
+    return 0;
+}   */
+
+//puts() :
+/*{
+    char a[100];
+    gets(a);
+    printf("You entered: ");
+    puts(a);
+    return 0;
+
+} */
+
+//scanf()
+/*{
+    int a;
+    scanf("%d", &a);
+    printf("You entered: %d", a);
+    return 0;
+
+} */
+
+/*{
+    int a,b;
+    printf("Enter two numbers: \n");
+    scanf("%d %d", &a,&b);
+    printf("\nSum : %d", a+b);
+    return 0;
+
+}*/
+
+//FORMATTED INPUT
+/*{
+    int x;
+    float num;
+    char text[20];
+    scanf("%d %f %s", &x,&num, text);
+} */
+
+/*{
+    int x,y;
+    char text[20];
+    scanf("%sd %d %*f %5s", &x,&y,text);
+    //input: 1234 5.7 elephant
+    printf("%d %d %s", x,y,text);
+    //output: 12 34 eleph 
+
+} */
+
+{
+    printf("The tree has %d apples.\n",22);
+    // The tree has 22 apples.
+    printf("\"Hello World!\"\n");
+    /* output: "Hello World!"
+}
+
+
+
+
+
+
+     
+
