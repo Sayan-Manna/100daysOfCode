41d4cdacca1628a265357d66355dd3296465d5e9
Sayan Manna <53704744+Sayan-Manna@users.noreply.github.com>
2020-01-29 00:07:41 +0530

    D2

---
+++ C/D2/UserInput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{  
+    int i,j,k;
+    printf("Enter 2 values");
+    scanf("%d",&i);
+    scanf("%d",&j);
+
+    k = i+j;
+    printf("The addition of %d and %d is %d",i,j,k);
+    return 0;
+
+}
